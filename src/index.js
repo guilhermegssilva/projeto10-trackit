5a84ff5e7824a1fbabf8758289a484f250bea7a4
Guilherme Silva <102394141+guilhermegssilva@users.noreply.github.com>
2022-05-30 11:55:30 -0300

    assets e contexto

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import App from "./componentes/App";
+import "./assets/reset.css";
+import "./assets/style.css";
+
+const root = document.querySelector(".root");
+ReactDOM.render(<App />, root);
